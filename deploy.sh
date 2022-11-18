9e5fe1df738e16fd8b631704bf64ab1c3662c21a
mateuscamillo <mateus.sze@outlook.com>
2022-11-18 00:18:29 +0000

    build, deploy e imagePullPolicy

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+shopt -s expand_aliases
+source /home/ubuntu/.bash_aliases
+
+kubectl apply -f ./kubernetes-manifests/redis.yaml
+kubectl apply -f ./kubernetes-manifests/emailservice.yaml
+kubectl apply -f ./kubernetes-manifests/productcatalogservice.yaml
+kubectl apply -f ./kubernetes-manifests/recommendationservice.yaml
+kubectl apply -f ./kubernetes-manifests/shippingservice.yaml
+kubectl apply -f ./kubernetes-manifests/checkoutservice.yaml
+kubectl apply -f ./kubernetes-manifests/paymentservice.yaml
+kubectl apply -f ./kubernetes-manifests/currencyservice.yaml
+kubectl apply -f ./kubernetes-manifests/cartservice.yaml
+kubectl apply -f ./kubernetes-manifests/frontend.yaml
+kubectl apply -f ./kubernetes-manifests/adservice.yaml
